776f79d287b1c4c92a35b37977f2ea964017e1de
ylan <ylan@agioe.com>
2023-08-22 14:20:22 +0800

    feature: manager of timers

---
+++ include/timecost_statistic_ros/timer.hpp
@@ -0,0 +1,145 @@
+#include <chrono>
+#include <string>
+#include <vector>
+#include <map>
+#include <mutex>
+#include <set>
+#include <iostream>
+// #include <stack>
+// using namespace std::chrono_literals;
+// using namespace std::chrono;
+
+
+namespace time_statistic_ros
+{
+using ClockType = std::chrono::system_clock;
+using TimePointT = std::chrono::time_point<ClockType>;
+using DurationT = std::chrono::microseconds;
+
+/**
+ * @brief
+ * @todo offer interface for setting log message stream
+ * @todo change to a template class for specifying duration type
+ * @todo use a stack to organize the waking up order of timers
+ */
+class TimerManager{
+ public:
+  TimerManager()
+    : log_info_(std::cout), log_warn_(std::cout), log_err_(std::cerr)
+  {};
+
+  // ~TimerManager();
+
+  bool registerTimer(const std::string &id) {
+    timers_.insert(std::pair<std::string, Timer>(id,{}));
+    return true;
+  }
+
+  inline bool startTimer(
+      const std::string& id,
+      const TimePointT* beginning=nullptr,
+      bool create_if_not_existed=true)
+  {
+    auto timer = timers_.find(id);
+    if(timer == timers_.end() && create_if_not_existed) {
+      // create a new timer with the id
+      timers_.insert(std::pair<std::string, Timer>(id,{}));
+      timer = timers_.find(id);
+    } else if (!create_if_not_existed) {
+      log_err_ << "Timer with ID \"" << id << "\" does not exist" << std::endl;
+      return false;
+    }
+
+    timer->second.ticking = true;
+    timer->second.beginning =
+        beginning ? *beginning : std::chrono::high_resolution_clock::now();
+
+    return true;
+  }
+
+  inline bool endTimer(
+      const std::string& id,
+      const TimePointT ending=std::chrono::high_resolution_clock::now())
+  {
+    auto timer = timers_.find(id);
+    if(timer == timers_.end()) {
+      log_err_ << "Timer with ID \"" << id << "\" does not exist" << std::endl;
+      return false;
+    } else if (!timer->second.ticking) {
+      log_err_ << "Timer with ID \"" << id << "\" is not ticking" << std::endl;
+      log_err_ << "Call startTimer with this ID before calling endTimer"
+               << std::endl;
+      return false;
+    }
+
+    auto& records = timer->second.records;
+    if(records.size() == max_timer_record_size_) {
+      ++max_timer_record_size_;
+    } else {
+      while(records.size() < max_timer_record_size_ - 1) {
+        records.push_back(nullptr);
+      }
+    }
+
+    records.push_back(new std::chrono::milliseconds(
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+            ending - timer->second.beginning)));
+
+    return true;
+  }
+
+  void flattenRecords(){
+    for(size_t i = 0; i < max_timer_record_size_; ++i){
+      log_info_ << "Record #" << i <<": " << std::endl << "\t";
+      for(const auto &timer : timers_) {
+        log_info_ << timer.first << ": ";
+        if(timer.second.records[i]==NULL) log_info_ << "nan | ";
+        else
+          log_info_ << std::chrono::duration_cast<std::chrono::microseconds>(
+              *timer.second.records[i]).count()*1e-3 << "ms | ";
+      }
+      log_info_ << std::endl;
+    }
+  }
+
+  /**
+   * @brief Disable all timers
+  */
+  void disableTimers(){
+    for(auto& timer : timers_){
+      timer.second.enable = false;
+    }
+  }
+
+  /**
+   * @brief Disable timers with IDs specified
+   * @param ids specified IDs of timers to disable
+   */
+  void disableTimers(const std::vector<std::string>& ids){
+    for(const auto& id : ids) {
+      if(timers_.find(id) != timers_.end()) {
+
+      }
+    }
+  }
+
+ private:
+
+  std::mutex mtx_;
+  size_t max_timer_record_size_{0};
+
+  std::ostream& log_info_;
+  std::ostream& log_warn_;
+  std::ostream& log_err_;
+
+  struct Timer{
+    bool ticking{false};
+    bool enable{false};
+    TimePointT beginning;
+    std::vector<std::chrono::milliseconds*> records;
+  };
+
+  std::map<std::string, Timer> timers_;
+};
+
+} // namespace time_statistic_ros
---
+++ test/test.cpp
@@ -0,0 +1,47 @@
+#include<unistd.h>
+
+#include "timecost_statistic_ros/timer.hpp"
+using namespace time_statistic_ros;
+using namespace std::chrono_literals;
+
+void foo1(){
+  std::cout << "foo1 running" << std::endl;
+  sleep(1);
+  // sleep(0.1111);
+}
+
+void foo2(){
+  std::cout << "foo2 running" << std::endl;
+  sleep(2);
+  // sleep(0.2222);
+}
+
+void foo3(){
+  std::cout << "foo3 running" << std::endl;
+  sleep(3);
+  // sleep(0.3333);
+}
+
+int main(int argc, char** argv) {
+  TimerManager timers;
+
+  for(int i=0; i < 2; ++i){
+    timers.startTimer("foo1_timer");
+    foo1();
+    timers.endTimer("foo1_timer");
+
+    timers.startTimer("foo2_timer");
+    foo2();
+    timers.endTimer("foo2_timer");
+
+    if(i==0) {
+      timers.startTimer("foo3_timer");
+      foo3();
+      timers.endTimer("foo3_timer");
+    }
+  }
+
+  timers.flattenRecords();
+
+  return 0;
+}
